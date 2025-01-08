05d7a621569143aef3ca53e8f162e0d62cf70910
LiTianXinEden <23018351@myrp.edu.sg>
2025-01-08 16:17:50 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+const KEY = "";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("batman");
+
+  // useEffect(() => {
+  //   const controller = new AbortController();
+  //   fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`, { signal: controller.signal }) 
+  //     .then((res) => res.json())
+  //     .then((data) => data.Response === "True" && setMovies(data.Search))
+  //     .catch((err) => console.log(err));
+  //   return () => controller.abort();
+  // }, [query]); //without the [], the setMovies which is the state is being re-rendered and then it keep calling the fetch
+
+  // function handleChange(e) {
+  //   setQuery(e.target.value);
+  // };
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(
+          `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+          {
+            signal: controller.signal,
+          }
+        );
+        const data = await response.json();
+        if (data.Response === "True") {
+          setMovies(data.Search);
+        }
+      } catch (error) {
+        console.error("Fetch error:", error.message);
+      }
+    };
+    fetchMovies();
+    return () => {
+      controller.abort();
+    };
+  }, [query]);
+
+
+
+
+  return (
+    <div>
+      <h1>Movies</h1>
+      <input type="text" placeholder="Search movies..." value={query} onChange={handleChange} />
+
+
+
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map((movie) => (
+            <tr key={movie.imdbID}>
+              <td>{movie.Title}</td>
+              <td>{movie.Year}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
